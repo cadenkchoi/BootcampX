55be1a5358277822281dd58f27795c1d4e5880f7
Caden Choi <cadenc.7@gmail.com>
2021-03-29 23:49:02 +0000

    completed queries 3 group by challenges

---
+++ 3_queries_group-by/12_large_cohort.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name AS cohort_name, count(students.name) AS student_count
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+HAVING count(students.name) >= 18
+ORDER BY count(students.name);
